dc6461f28f677ec6bc54ef23b720e4edee578334
Bruno-Morales <br1_morales@hotmail.com>
2022-06-05 15:40:04 -0300

    Terminando detalles de Vista.

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const port = 3000
+
+// vista publica
+const views = path.join(__dirname, 'public/views/')
+
+// rutas
+const httpRaiz = '/'
+const httpHome = '/Home'
+const httpLogin = '/Login'
+const homeHtml = 'Home.html'
+const LoginHtml = 'Login.html'
+
+const RegisterHtml = 'Register.html'
+const httpRegister = '/Register'
 
 
-app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(express.static(__dirname + "/public"));
+
+app.get(httpRaiz, (req, res) => {
+    res.sendFile(path.join(views, homeHtml))
+})
+
+app.get(httpHome, (req, res) => {
+    res.sendFile(path.join(views, homeHtml))
+})
+
+
+
+app.get(httpRegister, (req, res) => {
+    res.sendFile(path.join(views, RegisterHtml))
+})
+
+
+app.get(httpLogin, (req, res) => {
+    res.sendFile(path.join(views, LoginHtml))
+})
+
+app.get(httpHome, (req, res) => {
+    res.sendFile(path.join(views, homeHtml))
+})
+
+app.post(httpLogin, (req, res) => {
+    res.sendFile(path.join(views, LoginHtml))
+})
+
+app.post(httpHome, (req, res) => {
+    res.sendFile(path.join(views, homeHtml))
+})
+
+
 
 app.listen(3000 || process.env.PORT, () => console.log('El servidor se ha iniciado correctamente.'));
